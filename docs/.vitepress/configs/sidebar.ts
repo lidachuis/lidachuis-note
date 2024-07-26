41d62246ab3e62680b5937482e99422e481966b7
lidachuis <101002779+lidachuis@users.noreply.github.com>
2024-07-26 21:36:54 +0800

    add

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -5,7 +5,9 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
         {
             text: '测试',
             items: [
-                { text: '生信工具', link: '/tools/A1/seqkit' }
+                { text: '生信工具', link: '/tools/A1/seqkit' },
+                { text: '生信工具1', link: '/tools/A1/Nolebase' },
+                { text: '生信工具2', link: '/tools/A1/Nolebase集成' },
             ]
         }
     ]
